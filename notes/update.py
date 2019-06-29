529010de4ec820f2c0a9bc32c2bb2d937cfb9830
Matthew Katzman <matthew.katzman@cs.ox.ac.uk>
2019-06-29 10:05:16 +0100

    Testing update.py

--- notes/update.py
+++ notes/update.py
@@ -4,4 +4,4 @@ with open(sys.argv[1], "r+") as file:
 	old = file.read()
 	file.seek(0)
 	file.truncate()
-	file.write(old.replace("align=middle", "align=\"middle\"").replace("width=", "width=\"").replace("pt height=", "pt\" height=\"").replace("pt/>", "pt\"/>"))
+	file.write(old.replace("align=middle", "align=\"middle\"").replace("width=\"", "width=").replace("width=", "width=\"").replace("pt height=", "pt\" height=\"").replace("pt/>", "pt\"/>"))
