a585596100d7f7b1d56879833d6fa1069552fc1e
Matthew Katzman <matthew.katzman@cs.ox.ac.uk>
2019-06-29 09:47:09 +0100

    Added update

---
+++ notes/update.py
@@ -0,0 +1,7 @@
+import sys
+
+with open(sys.argv[1], "r+") as file:
+	old = file.read()
+	file.seek(0)
+	file.truncate()
+	file.write(old.replace("align=middle", "align=\"middle\"").replace("width=", "width=\"").replace("pt height=", "pt\" height=\"").replace("pt/>", "pt\"/>"))
